ba3df3d04ec21fea96cb99c124d9ffe0f7d62fa0
AngieZuGa <zunigaangelica871@gmail.com>
2025-07-24 18:25:29 -0600

    Mi primer commit Last.fm API

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  template: `
+    <div style="text-align:center; margin-top: 20px;">
+      <h1>API Last.fm - Búsqueda de Artistas</h1>
+      <input [(ngModel)]="artist" placeholder="Nombre del artista" />
+      <button (click)="buscarArtista()">Buscar</button>
+
+      <div *ngIf="resultado">
+        <h2>Resultado:</h2>
+        <pre>{{ resultado | json }}</pre>
+      </div>
+    </div>
+  `
 })
-export class App {
-  protected title = 'last-fm';
+export class AppComponent {
+  private http = inject(HttpClient);
+  artist: string = '';
+  resultado: any;
+
+  private API_KEY = '03ac5e67ccb398ec2895c9323167cc62';
+
+  buscarArtista() {
+    const url = `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${this.artist}&api_key=${this.API_KEY}&format=json`;
+
+    this.http.get(url).subscribe(data => {
+      this.resultado = data;
+    });
+  }
 }
